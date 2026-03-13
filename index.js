4010c6d7724635596cb9a4a65fd1536f9dbe39ef
jayyuvax <jayyuvax@gmail.com>
2026-03-14 01:37:29 +0530

    created config folder  for db

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import express, { json } from "express"
-import mongoose from "mongoose"
+import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import connectDB from "./config/db.js"
 import questionRoutes from "./routes/questionRoutes.js"
 
 dotenv.config()
@@ -9,19 +9,15 @@ dotenv.config()
 const app = express()
 
 app.use(cors())
-app.use(json())
+app.use(express.json())
 
-const PORT = process.env.PORT || 5000
-const MONGO_URI = process.env.MONGO_URI
-
-mongoose.connect(MONGO_URI)
-.then(() => {
-    console.log("MongoDB connected")
-
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`)
-    })
-})
-.catch(err => console.log(err))
+// connect database
+connectDB()
 
 app.use("/api/questions", questionRoutes)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})

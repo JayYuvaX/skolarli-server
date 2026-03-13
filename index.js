29fe3dc1d07a16ee6afbfc8b2102adb2d6fe638d
jayyuvax <jayyuvax@gmail.com>
2026-03-13 13:44:42 +0530

    render deploy error added mongoose + added correctAnswer in question schema

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import express, { json } from "express"
-import { connect } from "mongoose"
+import mongoose from "mongoose"
 import cors from "cors"
-import { config } from "dotenv"
+import dotenv from "dotenv"
 import questionRoutes from "./routes/questionRoutes.js"
 
+dotenv.config()
+
 const app = express()
-app.use(cors())
-config()
 
 app.use(cors())
 app.use(json())
@@ -14,12 +14,14 @@ app.use(json())
 const PORT = process.env.PORT || 5000
 const MONGO_URI = process.env.MONGO_URI
 
-connect(MONGO_URI)
-    .then(() => console.log("MongoDB connected"))
-    .catch(err => console.log(err))
+mongoose.connect(MONGO_URI)
+.then(() => {
+    console.log("MongoDB connected")
+
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`)
+    })
+})
+.catch(err => console.log(err))
 
 app.use("/api/questions", questionRoutes)
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
--- models/Question.js
+++ models/Question.js
@@ -1,8 +1,10 @@
 import { Schema, model } from "mongoose"
 
 const QuestionSchema = new Schema({
-question: String,
-options: [String]
+    question: String,
+    options: [String],
+    correctAnswer: Number
+
 })
 
 export default model("Question", QuestionSchema)
